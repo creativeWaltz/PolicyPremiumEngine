886a54659c7a5974860b9e12c6293e07a1133935
corcoranmam <71404312+creativeWaltz@users.noreply.github.com>
2021-03-21 16:49:03 +0000

    vessel-unit-tests-1

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,31 @@
 import unittest
+import vessel
 
 
-class VesselTests(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class VesselCreationTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.t_vessel = vessel.Vessel("HMS Ship", "91234567", 10_000_000)
+
+    def test_asset_creation(self):
+        """test name of object is correct"""
+        self.assertEqual(self.t_vessel.name, "HMS Ship")
+        self.assertEqual(self.t_vessel.value, 10_000_000)
+        self.assertEqual(self.t_vessel.imo, "91234567")
+        self.assertEqual(self.t_vessel.owned, True)
+        self.assertEqual(self.t_vessel.version, 1)
+
+    def test_asset_owned(self):
+        """test the asset ownership is True"""
+        pass
+
+
+    def test_version_number_1_at_creation(self):
+        """check version number is one when created"""
+        pass
+
+
+
+
 
 
 if __name__ == '__main__':
--- vessel.py
+++ vessel.py
@@ -8,16 +8,21 @@ class Vessel:
         self.imo = imo
         self.value = value
         self.owned = True
+        self.version = 1
         self.created_at_utc = datetime.datetime.utcnow()
 
     def sell(self):
         self.owned = False
+        self.version += 1
 
     def update_name(self, name):
         self.name = name
+        self.version += 1
 
     def update_imo(self, imo):
         self.imo = imo
+        self.version += 1
 
     def update_value(self, value):
         self.value = value
+        self.version += 1
